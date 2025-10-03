f341d51387051e1e0e1c231598a461dcb6f8b502
mmswflow-upb <mohamad_mario.sakka@stud.fils.upb.ro>
2025-10-03 14:36:05 +0300

    Adding readme

---
+++ Lab 1/lab1_1.py
@@ -0,0 +1,15 @@
+alphabet = []
+
+def find_alphabet(string):
+    for char in string:
+        char_exists = False
+        for symbol in alphabet:
+            if char == symbol:
+                char_exists = True
+                break
+        if not char_exists:
+            alphabet.append(char)
+    return alphabet
+
+print(find_alphabet("ATTTCGCCGATA"))
+        
---
+++ README.md
@@ -0,0 +1,1 @@
+# A repository for the Bioinformatics lab.
